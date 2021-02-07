4ea5dec3b6a13078a24676d16346d4679b09a857
weed <b.stromberg@data-systems.de>
2021-02-07 14:30:52 +0100

    ?

---
+++ map/tileset-basechip-pipo.tsx
@@ -0,0 +1,134 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="tileset-basechip-pipo" tilewidth="32" tileheight="32" tilecount="1064" columns="8">
+ <image source="tileset-basechip-pipo.png" width="256" height="4256"/>
+ <tile id="16">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="17">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="163">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="289">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="324">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="717">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="718">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="719">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="725">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="726">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="727">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="733">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="734">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="735">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="741">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="742">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="743">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="749">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="750">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="751">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="757">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="758">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="759">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="829">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="830">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="831">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+</tileset>
---
+++ map/tileset-lagerfeuer.tsx
@@ -0,0 +1,88 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="tileset-lagerfeuer" tilewidth="32" tileheight="32" tilecount="54" columns="18">
+ <image source="tileset-lagerfeuer.png" width="576" height="96"/>
+ <tile id="2">
+  <animation>
+   <frame tileid="2" duration="100"/>
+   <frame tileid="4" duration="100"/>
+   <frame tileid="6" duration="100"/>
+   <frame tileid="8" duration="100"/>
+   <frame tileid="10" duration="100"/>
+   <frame tileid="12" duration="100"/>
+   <frame tileid="14" duration="100"/>
+   <frame tileid="16" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="3">
+  <animation>
+   <frame tileid="3" duration="100"/>
+   <frame tileid="5" duration="100"/>
+   <frame tileid="7" duration="100"/>
+   <frame tileid="9" duration="100"/>
+   <frame tileid="11" duration="100"/>
+   <frame tileid="13" duration="100"/>
+   <frame tileid="15" duration="100"/>
+   <frame tileid="17" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="20">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+  <animation>
+   <frame tileid="20" duration="100"/>
+   <frame tileid="22" duration="100"/>
+   <frame tileid="24" duration="100"/>
+   <frame tileid="26" duration="100"/>
+   <frame tileid="28" duration="100"/>
+   <frame tileid="30" duration="100"/>
+   <frame tileid="32" duration="100"/>
+   <frame tileid="34" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="21">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+  <animation>
+   <frame tileid="21" duration="100"/>
+   <frame tileid="23" duration="100"/>
+   <frame tileid="25" duration="100"/>
+   <frame tileid="27" duration="100"/>
+   <frame tileid="29" duration="100"/>
+   <frame tileid="31" duration="100"/>
+   <frame tileid="33" duration="100"/>
+   <frame tileid="35" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="38">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+  <animation>
+   <frame tileid="38" duration="100"/>
+   <frame tileid="40" duration="100"/>
+   <frame tileid="42" duration="100"/>
+   <frame tileid="44" duration="100"/>
+   <frame tileid="46" duration="100"/>
+   <frame tileid="48" duration="100"/>
+   <frame tileid="50" duration="100"/>
+   <frame tileid="52" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="39">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+  <animation>
+   <frame tileid="39" duration="100"/>
+   <frame tileid="41" duration="100"/>
+   <frame tileid="43" duration="100"/>
+   <frame tileid="45" duration="100"/>
+   <frame tileid="47" duration="100"/>
+   <frame tileid="49" duration="100"/>
+   <frame tileid="51" duration="100"/>
+   <frame tileid="53" duration="100"/>
+  </animation>
+ </tile>
+</tileset>
